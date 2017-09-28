239d250792bebd0e1d09e2907a2c0e4bdee19fa4
Tanner Smith <tsmit343@uncc.edu>
2017-09-28 14:56:33 -0400

    test

--- public/assets/js/cities.js
+++ public/assets/js/cities.js
@@ -119,65 +119,6 @@ $(document).ready(function() {
         
           
         
-    });    
-  
-    // A function to handle what happens when the form is submitted to create a new Author
-    function handleAgentSignUp(event) {
-      event.preventDefault();
-      // Don't do anything if the name fields hasn't been filled out
-      if (!nameInput.val().trim().trim()) {
-        return;
-      }
-      // Calling the upsertAuthor function and passing in the value of the name input
-      uploadAgent({
-        username: nameInput
-          .val()
-          .trim(),
-        password: passInput
-          .val()
-          .trim()
-      });
-    }
-  
-    function handleAgentSignIn(event) {
-        event.preventDefault();
-        // Don't do anything if the name fields hasn't been filled out
-        if (!nameInput.val().trim().trim()) {
-          return;
-        }
-        // Calling the upsertAuthor function and passing in the value of the name input
-        checkAgent({
-          username: nameInput
-            .val()
-            .trim(),
-          password: passInput
-            .val()
-            .trim()
-        });
-      }
+    });
 
-    // A function for creating an author. Calls getAuthors upon completion
-    function uploadAgent(agentData) {
-      $.post("/api/agent", agentData)
-        .then(function(response){
-            if (response){
-                window.location.href="/questions/"
-            }
-            else {
-                alert("acct error");
-            }
-        });
-    }
-    
-    function checkAgent(agentData){
-      $.get("/api/agent/:id", agentData)
-        .then(function(response){
-            if (response){
-                window.location.href="/questions/"
-            }
-            else {
-                alert("acct error");
-            }
-        });
-    }
-  });
+})
